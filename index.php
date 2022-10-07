fa12dd7c22d301339b06f2d4c156f7d061cfa28d
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2022-10-07 13:12:33 +0200

    On met la nouvelle version

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Coming soon</title>
+	<link rel="shortcut icon" type="image/x-icon" href="Logo_mini.png" />
+	<link rel="stylesheet" href="404.css">
+</head>
+<body>
+<div id="container">
+    <div class="content">
+        <h2>Il arrive</h2>
+        <h4>Ne vous inquiétez pas !</h4>
+        <p>Le site est en cours de construction. Il est bientôt finit. Vous pouvez bientôt gâter vos amis à poils</p>
+    </div>
+</div>
+<script src="404.js"></script>
+</body>
+</html>
